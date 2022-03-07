0a6496b3e9a3f85bda569cb973eeb9056cf7e1b3
eric <eric.thomas@formation-industries-fc.fr>
2022-03-07 16:25:34 +0100

    v6

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 char chainesaisie[255];
 char *decalerPremiereLettre();
 char *decalerToutesLesLettres();
-char *decalerToutesLesLettres(int);
+char *decalerToutesLesLettresAvecDirection(int);
 int decalage;
 char direction;
 char alphabet[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'};
@@ -72,7 +72,23 @@ char *decalerToutesLesLettres()
 }
 char *decalerToutesLesLettresAvecDecalage(int decalage)
 {
-    //TODO
+    int positiontrouve=0;
+    for(int s=0;s<strlen(chainesaisie);s++) {
+//1-récupérer le premier caractere de la chaine
+        char charS = chainesaisie[s];
+
+//2-le retrouver dans l'alphabet
+        for (int i = 0; i < strlen(alphabet); i++) {
+            if (alphabet[i] == charS) {
+                positiontrouve = i;
+                break;
+            }
+        }
+//3-Décaler de 1
+        chainesaisie[s] = alphabet[positiontrouve + 1];
+    }
+//4-Récupérer la chaine décalée
+    return chainesaisie;
 }
 
 int main() {
